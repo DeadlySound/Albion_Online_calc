05cedd3eb14070c4642f2a6481440301db952e25
Adam Lewandowski <64542253+DeadlySound@users.noreply.github.com>
2020-12-07 13:52:33 +0100

    Photon - Łączenie się z Master Server

---
+++ Assets/Scripts/PHOTONConnect.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PHOTONConnect : MonoBehaviour
+{
+    string game_version = "0.01";
+    void Start()
+    {
+        Connect_to_Photon();
+    }
+
+    void Connect_to_Photon()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = game_version;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 200, 20), PhotonNetwork.NetworkClientState.ToString());
+    }
+}
