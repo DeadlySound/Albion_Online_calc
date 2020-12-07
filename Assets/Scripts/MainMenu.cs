b223fede5bdd58b6dca30313300be68065621fad
Adam Lewandowski <64542253+DeadlySound@users.noreply.github.com>
2020-12-07 14:18:12 +0100

    Tworzenie pokoju
    skrypty umożliwiające stworzenie / dołączenie do pokoi.
    
    Co-Authored-By: Rnbwf <68906476+Rnbwf@users.noreply.github.com>
    Co-Authored-By: Rychoc <43826078+Rychoc@users.noreply.github.com>

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public InputField room_name_input;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void On_click_create_room()
+    {
+        PHOTONConnect.Click_create_room(room_name_input.text, 1);
+    }
+    public void On_click_join_room()
+    {
+        PHOTONConnect.Click_join_room(room_name_input.text, 1);
+    }
+}
--- Assets/Scripts/PHOTONConnect.cs
+++ Assets/Scripts/PHOTONConnect.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class PHOTONConnect : MonoBehaviour
 {
     string game_version = "0.01";
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
         Connect_to_Photon();
@@ -20,5 +24,21 @@ public class PHOTONConnect : MonoBehaviour
     private void OnGUI()
     {
         GUI.Label(new Rect(0, 0, 200, 20), PhotonNetwork.NetworkClientState.ToString());
+    }
+    static public void Click_create_room(string room_name, int level)
+    {
+        if (room_name.Length >= 1)
+        {
+            PhotonNetwork.CreateRoom(room_name, new Photon.Realtime.RoomOptions { MaxPlayers = 5 }, null);
+            PhotonNetwork.LoadLevel(level);
+        }
+    }
+    static public void Click_join_room(string room_name, int level)
+    {
+        if (room_name.Length >= 1)
+        {
+            PhotonNetwork.JoinRoom(room_name, null);
+            PhotonNetwork.LoadLevel(level);
+        }
     }
 }
