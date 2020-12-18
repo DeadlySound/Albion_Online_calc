a3e5cac45cb35d9451b533140e7a0a8c9a964798
Adam Lewandowski <64542253+DeadlySound@users.noreply.github.com>
2020-12-18 16:03:43 +0100

    Zatrzymanie powielania obiektu
    Naprawiono automatyczne powielanie się obiektu Photon_Manager po wyjściu do menu głównego

---
+++ Assets/Scripts/CreatePhotonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePhotonManager : MonoBehaviour
+{
+    public GameObject photonManagerPrefab;
+    private void Awake ()
+    {
+        if (!FindObjectOfType<PHOTONConnect>())
+        {
+            Instantiate(photonManagerPrefab);
+        }
+    }
+}
