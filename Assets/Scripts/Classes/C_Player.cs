62c9138ba4470ebbd847cf7adb992b8711add210
RainBoff <68906476+RainBoff@users.noreply.github.com>
2021-01-10 22:35:28 +0100

    Dodanie klasy C_Player
    Player wywala errory z photona dlatego jest C_Player
    
    Co-Authored-By: Adam Lewandowski <64542253+DeadlySound@users.noreply.github.com>

---
+++ Assets/Scripts/Classes/C_Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class C_Player : MonoBehaviour
+{
+    public uint Gold = 5;
+    public uint PreistCardCount = 0;
+    public uint PhilosopherCardCount = 0;
+   //TODO dodac publicznego God GodForThisRound
+    
+
+    public void RiseBid() 
+    { }
+
+}
